a086e18d2d34b261a21bad52f1bdea1a0667e5ef
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-06-14 22:05:28 -0300

    Create project

---
+++ Wine Shop/Wine Shop/WineShopApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct WineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Wine Shop/Wine ShopTests/WineShopTests.swift
@@ -0,0 +1,5 @@
+import XCTest
+@testable import Wine_Shop
+
+final class WineShopTests: XCTestCase {
+}
