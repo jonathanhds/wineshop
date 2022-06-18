2d5b1850f8eb04dcec5910af0738686be05506bc
Jonathan Henrique de Souza <jonathan.henrique@icloud.com>
2022-06-17 21:31:57 -0300

    Map all products IDs

---
+++ Wine Shop/Wine Shop/Product/Product.swift
@@ -0,0 +1,11 @@
+let ALL_PRODUCTS_IDS = [
+	"wine.cabernet-sauvignon",
+	"wine.carmenere",
+	"wine.malbec",
+	"wine.merlot",
+	"wine.tempranillo",
+	"wine.pinot-noir",
+	"wine.syrah",
+	"wine.chardonnay",
+	"wine.sauvignon-blanc"
+]
